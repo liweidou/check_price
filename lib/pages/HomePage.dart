c3782354bf96618637e92af0efe768acfe01dfe9
tom <798192335@qq.com>
2020-05-18 17:16:56 +0800

    2020/5/18 17:16 添加ad

--- lib/pages/HomePage.dart
+++ lib/pages/HomePage.dart
@@ -2,16 +2,61 @@ import 'dart:io';
 
 import 'package:check_price/pages/SearchPage.dart';
 import 'package:check_price/pages/UploadPage.dart';
+import 'package:firebase_admob/firebase_admob.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
 
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
+const String testDevice = '33BE2250B43518CCDA7DE426D04EE232';
+
 class _HomePageState extends State<HomePage> {
+
+  static const MobileAdTargetingInfo targetingInfo = MobileAdTargetingInfo(
+    testDevices: testDevice != null ? <String>[testDevice] : null,
+    keywords: <String>['foo', 'bar'],
+    contentUrl: 'http://foo.com/bar.html',
+    childDirected: true,
+    nonPersonalizedAds: true,
+  );
+
+  BannerAd _bannerAd;
+
+  BannerAd createBannerAd() {
+    return BannerAd(
+      adUnitId: BannerAd.testAdUnitId,
+      size: AdSize.banner,
+      targetingInfo: targetingInfo,
+      listener: (MobileAdEvent event) {
+        print("BannerAd event $event");
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseAdMob.instance.initialize(appId: Platform.isAndroid
+        ? 'ca-app-pub-5426843524329045~3274164592'
+        : 'ca-app-pub-5426843524329045~5102800320');
+    _bannerAd = createBannerAd()..load();
+    _bannerAd ??= createBannerAd();
+    _bannerAd
+      ..load()
+      ..show(horizontalCenterOffset: 0, anchorOffset: 0);
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _bannerAd?.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
